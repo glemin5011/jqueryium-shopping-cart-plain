618f7dac7934534558f2574c00c5b595c6479ae4
Matej <matejpale@gmail.com>
2021-06-22 20:48:59 +0200

    Fixed decimal point in form and updatecart for new

--- js/application.js
+++ js/application.js
@@ -1,4 +1,4 @@
-//Functions 
+//Functions
 
 var calculateSubTotal = function (ele) {
   //var quantityItem = parseFloat($(ele).find(".quantity input").val());
@@ -6,13 +6,16 @@ var calculateSubTotal = function (ele) {
   var quantityItem = Number(`${$(ele).find(".quantity input").val()}`);
   //var priceItem = parseFloat($(ele).children(".price").text());
   //var priceItem = parseFloat(Math.round($(ele).children(".price").text() * 100) / 100).toFixed(2);
-  var priceItem = Number(`${$(ele).children(".price").text()}`.replace(/[^0-9.-]+/g,""));
-  
+  var priceItem = Number(
+    `${$(ele).children(".price").text()}`.replace(/[^0-9.-]+/g, "")
+  );
 
   var subTotal = quantityItem * priceItem;
   if (subTotal >= 0) {
-  $(ele).children(".subtotal").html(`$${parseFloat(Math.round(subTotal * 100) / 100).toFixed(2)}`);
-}
+    $(ele)
+      .children(".subtotal")
+      .html(`$${parseFloat(Math.round(subTotal * 100) / 100).toFixed(2)}`);
+  }
 
   return subTotal;
 };
@@ -21,49 +24,50 @@ var sum = function (acc, x) {
   return acc + x;
 };
 
-var updateTotalCart = function () { 
-var allSubTotals = [];
+var updateTotalCart = function () {
+  var allSubTotals = [];
 
   $("tbody tr").each(function (i, ele) {
     var subTotal = calculateSubTotal(ele);
-      allSubTotals.push(subTotal || 0); // push result of subtotal function to array or a zero if NaN
+    allSubTotals.push(subTotal || 0); // push result of subtotal function to array or a zero if NaN
   });
 
-  if(allSubTotals.length == 0) {
+  if (allSubTotals.length == 0) {
     $("#totalCart").html(`$--.--`);
   } else {
-  var totalCart = allSubTotals.reduce(sum);
-  $("#totalCart").html(`$${parseFloat(Math.round(totalCart * 100) / 100).toFixed(2)}`);
-}
-}
-
+    var totalCart = allSubTotals.reduce(sum);
+    $("#totalCart").html(
+      `$${parseFloat(Math.round(totalCart * 100) / 100).toFixed(2)}`
+    );
+  }
+};
 
 //Execution sequence
 $(document).ready(function () {
   updateTotalCart();
-// remove button
-  $("body").on('click', '.remove', function (event) {
-    $(this).closest('tr').remove();
+  // remove button
+  $("body").on("click", ".remove", function (event) {
+    $(this).closest("tr").remove();
     updateTotalCart();
   });
 
   //update shopping cart
   var timeout;
-  $("tr input").on('input', function () {
+  $("body").on("input", "tr input", function () {
     clearTimeout(timeout);
     timeout = setTimeout(function () {
       updateTotalCart();
     }, 500);
-  })
+  });
 
   //Add item code
-  $("#addItem").on('submit', function (event) {
+  $("#addItem").on("submit", function (event) {
     event.preventDefault();
     var item = $(this).children(".item").val();
     var price = $(this).children(".price").val();
-  
-  //inject into DOM
-  $('tbody').append(`<tr> 
+
+    //inject into DOM
+    $("tbody").append(`<tr> 
   <td class="item">${item}</td>
   <td class="price">$${price}</td>
   <td class="quantity">
@@ -72,9 +76,7 @@ $(document).ready(function () {
   </td>
   <td class="subtotal"></td>
   </tr>
-  `)
-  updateTotalCart();  
+  `);
+    updateTotalCart();
   });
 });
-
-
